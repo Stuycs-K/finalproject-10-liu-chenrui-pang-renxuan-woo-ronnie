fa8cd18b093447dbb062decc232db0fdbc4a0c1e
Chenrui Liu <cliu50@stuy.edu>
2025-05-20 14:35:14 -0500

    Added notes about the algorithm

--- encode.java
+++ encode.java
@@ -2,7 +2,7 @@ public class Decode {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Please enter plaintext.");
-        } 
+        }
         else {
             String input = args[0];
             System.out.println(converter(input));
@@ -10,15 +10,24 @@ public class Decode {
     }
 
     public static String converter(String text) {
-        String result = "";
+        String result = "++++++++++";
         for (char c : text.toCharArray()) {
             int asciiValue = (int) c;
-            for (int i = 0; i < asciiValue; i++) {
+            for (int i = 0; i < asciiValue / 10; i++) {
                 result += "+";
             }
-            result += ".";
             result += ">";
         }
+        // go back "<" depending on length of text to cell 0 to decrement.
+        // increment the remainders of the ascii values and then output.
         return result;
     }
 }
+
+// Note to self:
+// "Hi" becomes ++++++++++ [> +++++++ > ++++++++++ << - ] > ++ . > +++++ .
+// Use this algorithm
+
+// Can set common denominator to 10 since normal plaintext should go from 32 to 126 and then increment the last digit.
+
+// So the brainfuck will always start with ++++++++++ and then loop depending on the plaintext
