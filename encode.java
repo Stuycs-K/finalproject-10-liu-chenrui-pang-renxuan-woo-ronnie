08ec7f8110a188f5d577ddb00774386a1c71c29b
Ronnie Woo <rwoo50@stuy.edu>
2025-05-20 15:32:27 -0400

    implemented first 5 commands

--- encode.java
+++ encode.java
@@ -5,7 +5,29 @@ public class encode{
     }
     else{
       String input = args[0];
-      System.out.println(input);
+      char[] array = new char[30000];
+      int pointer = 0;
+      for (int i = 0; i < input.length(); i++){
+        char cmd = input.charAt(i);
+        if (cmd == '>'){
+          pointer++;
+        }
+        if (cmd == '<'){
+          pointer--;
+        }
+        if (cmd == '+'){
+          array[pointer]++;
+        }
+        if (cmd == '-'){
+          array[pointer]--;
+        }
+        if (cmd == '.'){
+          System.out.println(array[pointer]);
+        }
+      }
     }
   }
 }
+
+//tests
+//make encode ARGS="[->+<]"
