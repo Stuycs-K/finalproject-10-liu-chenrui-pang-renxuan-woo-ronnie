ab9efeac03ef3a77e26db6053f017af81bac63a9
Chenrui <liuchenrui2@gmail.com>
2025-05-19 22:49:06 -0400

    Fix encode

--- encode.java
+++ encode.java
@@ -1,23 +1,24 @@
-public class decode{
-    public static void main(String[] args){
-        if (args.length == 0){
-            System.out.println("Please enter plaintext.")
-        }
-        else{
+public class Decode {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please enter plaintext.");
+        } 
+        else {
             String input = args[0];
+            System.out.println(converter(input));
         }
     }
 
-    public static String converter(String text){
-        result = "";
-        for(char c : text.toCharArray()){
-            int asciiValue = int(c);
-
-            for (int i = 0; i < asciiValue; i++){
+    public static String converter(String text) {
+        String result = "";
+        for (char c : text.toCharArray()) {
+            int asciiValue = (int) c;
+            for (int i = 0; i < asciiValue; i++) {
                 result += "+";
             }
+            result += ".";
+            result += ">";
         }
-
-        result += ",>";
+        return result;
     }
 }
