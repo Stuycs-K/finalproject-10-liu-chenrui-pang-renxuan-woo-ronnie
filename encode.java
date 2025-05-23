35d8c08f0071286113405f07fe69413e1b7e64d2
Ronnie Woo <rwoo50@stuy.edu>
2025-05-23 15:36:10 -0400

    finished loop code with [ and ]

--- encode.java
+++ encode.java
@@ -9,48 +9,60 @@ public class encode{
       String input = args[0];
       char[] array = new char[30000];
       int pointer = 0;
-      for (int i = 0; i < input.length(); i++){
-        char cmd = input.charAt(i);
-        if (cmd == '>'){
-          pointer++;
-          if (pointer > array.length){
-            System.out.println("RANGE ERROR");
-            break;
-          }
-        }
-        if (cmd == '<'){
-          pointer--;
-          if (pointer < 0){
-            System.out.println("RANGE ERROR");
-            break;
-          }
-        }
-        if (cmd == '+'){
-          array[pointer]++;
-          if (array[pointer] == 256){
-            array[pointer] = 0;
-          }
-        }
-        if (cmd == '-'){
-          array[pointer]--;
-          if (array[pointer] == -1){
-            array[pointer] = 255;
-          }
-        }
-        if (cmd == '.'){
-          System.out.print(array[pointer]);
-        }
-        if (cmd == ','){
-          Scanner s = new Scanner(System.in);
-          System.out.print("Enter a character to store in the array: ");
-          array[pointer] = s.next().charAt(0);
-        }
-      }
-      System.out.println("");
+      execute(input, array, pointer);
     }
   }
-}
 
+  public static int execute(String input, char[] array, int pointer){
+    for (int i = 0; i < input.length(); i++){
+      char cmd = input.charAt(i);
+      if (cmd == '>'){
+        pointer++;
+        if (pointer > array.length){
+          System.out.println("RANGE ERROR");
+          break;
+        }
+      }
+      if (cmd == '<'){
+        pointer--;
+        if (pointer < 0){
+          System.out.println("RANGE ERROR");
+          break;
+        }
+      }
+      if (cmd == '+'){
+        array[pointer]++;
+        if (array[pointer] == 256){
+          array[pointer] = 0;
+        }
+      }
+      if (cmd == '-'){
+        array[pointer]--;
+        if (array[pointer] == -1){
+          array[pointer] = 255;
+        }
+      }
+      if (cmd == '.'){
+        System.out.print(array[pointer]);
+      }
+      if (cmd == ','){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a character to store in the array: ");
+        array[pointer] = s.next().charAt(0);
+      }
+      if (cmd == '['){
+        String subInput = input.substring(i + 1);
+        subInput = subInput.substring(0, subInput.indexOf(']'));
+        while (array[pointer] != 0){
+          pointer = execute(subInput, array, pointer);
+        }
+        i += subInput.length() + 1;
+      }
+    }
+    System.out.println("");
+    return pointer;
+  }
+}
 //tests
 //make encode ARGS="+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+.+.+."
 //output: "ABCD"
